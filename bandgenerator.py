cd923a7ff0b3a6788f6664a40e135011160b3962
soeradj <soeradj.mahabiersing@gmail.com>
2024-05-23 18:42:24 +0200

    day 1 completed

---
+++ bandgenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to bandgenerator")
+name_1 = input("what is the city you were born in\n")
+
+name_2 = input("what is the name of your first pet\n")
+
+print("Your bandname could be:"+ name_1 + " " + name_2)
